82af8d70395297eeaaaac309d30aa66f028ecb51
dylannelkin <pandas9D!>
2019-02-12 17:07:11 -0500

    Added player model, with player states and other variables

--- SimpleFighter/Assets/Scripts/PlayerModel.cs
+++ SimpleFighter/Assets/Scripts/PlayerModel.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class PlayerModel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    #region Public Variables
+    
+    public PlayerState State = PlayerState.Idle; //The current state of the player
+    public int Health = 10; //How much health the player has
+    public int Stamina = 10; //How much stamina the player has
+    public int PlayerIndex;
+    
+    #endregion
 
-    // Update is called once per frame
+    #region Updates
+    
     void Update()
     {
-        
+        //Do specific things depending on our state
+        switch (State)
+        {
+             default:
+                 break;
+        }
     }
+    
+    #endregion
 }
+
+//Used to determine what our player is currently doing so that actions and animations don't conflict
+public enum PlayerState
+{
+    Walking,
+    Jumping,
+    Striking,
+    Grabbing,
+    Blocking,
+    Damage,
+    Special,
+    Crouching,
+    Grounded,
+    Idle
+};
+
+
