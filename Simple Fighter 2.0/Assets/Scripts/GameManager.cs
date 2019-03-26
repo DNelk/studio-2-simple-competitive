edddbcee5785465e9f7d20a856812c869f3eb53a
cgbNYU <cgb316@nyu.edu>
2019-03-26 16:58:28 -0400

    Reimplemented Rewired Input Manager data

--- Simple Fighter 2.0/Assets/Scripts/GameManager.cs
+++ Simple Fighter 2.0/Assets/Scripts/GameManager.cs
@@ -65,7 +65,7 @@ public class GameManager : MonoBehaviour
             players[i].Model = model.AddComponent<PlayerModel>();
             players[i].View = playerGO.AddComponent<PlayerView>();
             players[i].Controller = controller.AddComponent<PlayerController>();
-            players[i].Controller.SetRewiredPlayer(i); //after creating controller, set the player profile
+            players[i].Controller.SetRewiredPlayer(i + 1); //after creating controller, set the player profile
         }
     }
     
