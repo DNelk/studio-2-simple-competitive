f0555eb85b51090ec696ca9a245e07a421835bd5
cgbNYU <cgb316@nyu.edu>
2019-03-26 17:40:36 -0400

    Added skeletons of functions to Controller

--- Simple Fighter 2.0/Assets/Scripts/PlayerController.cs
+++ Simple Fighter 2.0/Assets/Scripts/PlayerController.cs
@@ -30,10 +30,65 @@ public class PlayerController : MonoBehaviour
     }
     
     #region Input Functions
-
+    
+    //Check for inputs during moveable states
     private void MoveCheck()
     {
-       
+        if (rewiredPlayer.GetAxisRaw("HorizontalMovement") != 0)
+        {
+            //switch to walking
+        }
+
+        if (rewiredPlayer.GetAxisRaw("HorizontalMovement") == 0)
+        {
+            //stop walking and switch to idle
+        }
+    }
+    
+    //Check for Strike
+    private void StrikeCheck()
+    {
+        if (rewiredPlayer.GetButtonDown("Strike"))
+        {
+            //switch to Strike
+        }
+    }
+    
+    //Check for Grab
+    private void GrabCheck()
+    {
+        if (rewiredPlayer.GetButtonDown("Grab"))
+        {
+            //switch to Grab
+        }
+    }
+    
+    //Check for Block
+    private void BlockCheck()
+    {
+        if (rewiredPlayer.GetButtonDown("Block"))
+        {
+            //switch to Block
+        }
+
+        if (rewiredPlayer.GetButtonUp("Block"))
+        {
+            //stop blocking, switch to Idle
+        }
+    }
+    
+    //Check for Tech
+    private void TechCheck()
+    {
+        if (rewiredPlayer.GetAxisRaw("HorizontalMovement") != 0)
+        {
+            //switch to tech roll and go in the correct direction
+        }
+        
+        else if (rewiredPlayer.GetAxisRaw("VerticalMovement") != 0)
+        {
+            //if up, Tech Stand
+        }
     }
     #endregion
 }
