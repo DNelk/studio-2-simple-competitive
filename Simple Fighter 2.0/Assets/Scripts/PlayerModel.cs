03019f9557829588b5bf346c1e8f007fe0801301
dylannelkin <pandas9D!>
2019-03-26 17:28:27 -0400

    Added: PlayerStates in PlayerModel

--- Simple Fighter 2.0/Assets/Scripts/PlayerModel.cs
+++ Simple Fighter 2.0/Assets/Scripts/PlayerModel.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerModel : MonoBehaviour
 {
-    
+
     #region Private Variables
+
     //Current hitpoints
     private int currentHitPoints;
-    
+
+    private StateMachine<PlayerModel> stateMachine;
+
     #endregion
-    
+
     #region Public Variables
-    
+
     //Character Statistics
     [Range(1, 5)] public int MaxHitPoints;
     [Range(1, 50)] public float MoveSpeed;
@@ -24,17 +27,46 @@ public class PlayerModel : MonoBehaviour
     [Range(1, 10)] public int GrabRecoveryFrames;
     [Range(1, 10)] public int BlockStartupFrames;
     [Range(1, 10)] public int BlockRecoveryFrames;
+
     #endregion
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Initialize state machine
+        stateMachine = new StateMachine<PlayerModel>(this);
+        stateMachine.TransitionTo<Idle>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        stateMachine.Update();
     }
+
+    public StateMachine<PlayerModel>.State GetState()
+    {
+        return stateMachine.CurrentState;
+    }
+
+    #region States
+
+    //Base Player State
+    private class PlayerState : StateMachine<PlayerModel>.State
+    {
+        private int timer;
+
+        public virtual void CooldownAnimationEnded(){}     
+    }
+
+    private class Walking : PlayerState{}
+    private class Striking : PlayerState{}
+    private class Blocking : PlayerState{}
+    private class Grabbing : PlayerState{}
+    private class Falling : PlayerState{}
+    private class Grounded : PlayerState{}
+    private class Rolling : PlayerState{}
+    private class Idle : PlayerState{}
+    private class Victory : PlayerState{}
+    #endregion
 }
