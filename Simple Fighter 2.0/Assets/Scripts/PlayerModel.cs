76b14cc67e93b90d458aee27a4adc1eff2bf798c
cgbNYU <cgb316@nyu.edu>
2019-03-26 15:48:13 -0400

    Added public and private player stat variables to model

--- Simple Fighter 2.0/Assets/Scripts/PlayerModel.cs
+++ Simple Fighter 2.0/Assets/Scripts/PlayerModel.cs
@@ -4,6 +4,28 @@ using UnityEngine;
 
 public class PlayerModel : MonoBehaviour
 {
+    
+    #region Private Variables
+    //Current hitpoints
+    private int currentHitPoints;
+    
+    #endregion
+    
+    #region Public Variables
+    
+    //Character Statistics
+    [Range(1, 5)] public int MaxHitPoints;
+    [Range(1, 50)] public float MoveSpeed;
+    [Range(1, 10)] public int StrikeStartupFrames;
+    [Range(1, 10)] public int StrikeActiveFrames;
+    [Range(1, 10)] public int StrikeRecoveryFrames;
+    [Range(1, 10)] public int GrabStartupFrames;
+    [Range(1, 10)] public int GrabActiveFrames;
+    [Range(1, 10)] public int GrabRecoveryFrames;
+    [Range(1, 10)] public int BlockStartupFrames;
+    [Range(1, 10)] public int BlockRecoveryFrames;
+    #endregion
+    
     // Start is called before the first frame update
     void Start()
     {
