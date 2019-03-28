e6f7075b20df0e1d2fa83cfa66c1b2c1c9aa9c1e
Dylan Nelkin <dylannelkin@gmail.com>
2019-03-28 16:29:23 -0400

    Moved: Animation folder to Resources/Animation Edit: PlayerView now creates and assigns animator

--- Simple Fighter 2.0/Assets/Scripts/PlayerView.cs
+++ Simple Fighter 2.0/Assets/Scripts/PlayerView.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class PlayerView : MonoBehaviour
 {
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = gameObject.AddComponent<Animator>();
+        animator.runtimeAnimatorController = Resources.Load<RuntimeAnimatorController>("Animations/Player");
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,6 @@ public class PlayerView : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       animator.SetTrigger("isStriking");
     }
 }
