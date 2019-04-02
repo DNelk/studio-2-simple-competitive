cb873bbbd971162f817e580f1813891b89c7887a
cgbNYU <cgb316@nyu.edu>
2019-04-02 15:28:25 -0400

    Eventmanager added

---
+++ Simple Fighter 2.0/Assets/Scripts/Events.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Fill this with Events which are subclasses of GameEvent
+public class Events : MonoBehaviour
+{
+    
+}
