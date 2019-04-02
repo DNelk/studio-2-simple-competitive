7234b7c6fb565dd9a098c2e7333fa65194c0459b
cgbNYU <cgb316@nyu.edu>
2019-04-02 16:25:07 -0400

    Added various events

--- Simple Fighter 2.0/Assets/Scripts/Events.cs
+++ Simple Fighter 2.0/Assets/Scripts/Events.cs
@@ -5,5 +5,85 @@ using UnityEngine;
 //Fill this with Events which are subclasses of GameEvent
 public class Events : MonoBehaviour
 {
+    //Animation Change Event
+    //Fired from Model and received by View
+    public class AnimationChange : GameEvent
+    {
+        public string AnimTrigger { get; }
+        public int PlayerIndex { get; }
+
+        public AnimationChange(string animTrigger, int playerIndex)
+        {
+            AnimTrigger = animTrigger;
+            PlayerIndex = playerIndex;
+        }
+    }
+    
+    //Translate Event
+    public class TranslatePos : GameEvent
+    {
+        public float Speed { get; }
+        public float RawAxis { get; }
+        public int PlayerIndex { get; }
+
+        public TranslatePos(float rawAxis, float speed)
+        {
+            Speed = speed;
+            RawAxis = rawAxis;
+        }
+    }
+    
+    //Get Hit Event
+    public class HitOpponent : GameEvent
+    {
+        public int PlayerIndex { get; }
+
+        public HitOpponent(int playerIndex)
+        {
+            PlayerIndex = playerIndex;
+        }
+    }
+    
+    //Health Changed Event
+    public class HealthChanged : GameEvent
+    {
+        public int NewHealth { get; }
+        public int PlayerIndex { get; }
+
+        public HealthChanged(int newHealth, int playerIndex)
+        {
+            NewHealth = newHealth;
+            PlayerIndex = playerIndex;
+        }
+    }
+    
+    //Process Input Event
+    public class ProcessInput : GameEvent
+    {
+        public PlayerController.inputState NewInput { get; }
+        public float Value { get; }
+        public int PlayerIndex { get; }
+
+        public ProcessInput(PlayerController.inputState newInput, int playerIndex)
+        {
+            NewInput = newInput;
+            PlayerIndex = playerIndex;
+        }
+
+        public ProcessInput(PlayerController.inputState newInput, float value, int playerIndex)
+        {
+            NewInput = newInput;
+            Value = value;
+            PlayerIndex = playerIndex;
+        }
+    }
+    
+    //Game End Event
+    public class GameEnd : GameEvent {}
+    
+    public class TimeOut : GameEvent {}
+    
+    public class Pause : GameEvent {}
+    
     
 }
