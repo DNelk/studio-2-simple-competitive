a8a377669280eb46d58302a5a66fcc5e9378ffae
palmmyvillage <43654403+palmmyvillage@users.noreply.github.com>
2019-04-16 16:44:09 -0400

    general check in

--- Simple Fighter 2.0/Assets/Scripts/palmmy's UI Code/TitleExplosion.cs
+++ Simple Fighter 2.0/Assets/Scripts/palmmy's UI Code/TitleExplosion.cs
@@ -17,7 +17,7 @@ public class TitleExplosion : MonoBehaviour
         Vector3 CenterModifier = new Vector3(0, 0f, 0f);
 
         rb.useGravity = false;
-        rb.AddExplosionForce(3000f, transform.parent.position + CenterModifier, 2000f, 3.0f);
+        rb.AddExplosionForce(3000f, transform.parent.position + CenterModifier, 150f, 3.0f);
     }
 
     // Update is called once per frame
